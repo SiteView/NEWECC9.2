5744f5250832c6fcf637b0fb3371170b1e4cee42
yufangabc123 <fang.yu@dragonflow.com>
2012-09-26 14:15:23 +0800

    邮件设置>模板设置yufang

--- ecc/src/SiteView/ecc/Modle/EmailModle.java
+++ ecc/src/SiteView/ecc/Modle/EmailModle.java
@@ -3,22 +3,10 @@ package SiteView.ecc.Modle;
 import Siteview.Api.BusinessObject;
 
 public class EmailModle {
-	public String MailTitle;
-	public String MailContent;
-	public String MailModle;
 	public BusinessObject bo;
 	
 	public EmailModle(BusinessObject bo) {
 		super();
-		this.bo = bo;
-	}
-
-	public EmailModle(String mailTitle, String mailContent, String mailModle,
-			BusinessObject bo) {
-		super();
-		MailTitle = mailTitle;
-		MailContent = mailContent;
-		MailModle = mailModle;
 		this.bo = bo;
 	}
 
@@ -29,29 +17,4 @@ public class EmailModle {
 	public void setBo(BusinessObject bo) {
 		this.bo = bo;
 	}
-
-	public String getMailTitle() {
-		return MailTitle;
-	}
-
-	public void setMailTitle(String mailTitle) {
-		MailTitle = mailTitle;
-	}
-
-	public String getMailContent() {
-		return MailContent;
-	}
-
-	public void setMailContent(String mailContent) {
-		MailContent = mailContent;
-	}
-
-	public String getMailModle() {
-		return MailModle;
-	}
-
-	public void setMailModle(String mailModle) {
-		MailModle = mailModle;
-	}
-	
 }
