204e452a162bad1d1d131d8f60cf41146859758e
zhonglihua <lihua.zhong@dragonflow.com>
2012-10-18 18:19:01 +0800

    Ecc视图

---
+++ ecc/src/SiteView/ecc/perspective.java
@@ -0,0 +1,14 @@
+package SiteView.ecc;
+
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+import SiteView.ecc.view.EccTreeControl;
+
+public class perspective implements IPerspectiveFactory {
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+		layout.addView(EccTreeControl.ID, IPageLayout.LEFT, 0.3f, layout.getEditorArea());
+	}
+
+}
